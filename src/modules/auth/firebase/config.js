6ee85bca57e1ecd46aa008e209e74c31b646a79d
Gaston Rodriguez <gaston.rodriguez@icloud.com>
2025-07-13 19:48:43 -0500

    Testing for production 1.9

--- src/modules/auth/firebase/config.js
+++ src/modules/auth/firebase/config.js
@@ -1,8 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-console.log("Using Firebase API key:", import.meta.env.VITE_FIREBASE_API_KEY);
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
